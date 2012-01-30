7e847ed6e3b000cefbd7eca4305de47fffa62334
Randi Hillerøe <silwing@gmail.com>
2012-01-30 11:11:44 +0100

    Added FileProcessingStrategy interface.

---
+++ src/week1/FileProcessingStrategy.java
@@ -0,0 +1,11 @@
+package week1;
+
+import java.io.*;
+
+/**
+ *
+ * @author Randi K. Hillerøe <silwing@gmail.com>
+ */
+public interface FileProcessingStrategy {
+	public String processFile(File f);
+}
