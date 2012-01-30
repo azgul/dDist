dc93c19bb10fa6673a017f858b936c072064d895
Randi Hillerøe <silwing@gmail.com>
2012-01-30 13:57:56 +0100

    HTTPServer now serving all files and refactored.

--- src/week1/HTTPServer.java
+++ src/week1/HTTPServer.java
@@ -154,49 +154,6 @@ public class HTTPServer {
 		
 		if(request.startsWith("GET"))
 			doGet(request);
-		
-		/*if (!request.startsWith("GET") || 
-				request.length()<14 || 
-				!(request.endsWith("HTTP/1.0") || request.endsWith("HTTP/1.1")) || 
-				request.charAt(4) != '/'){
-			errorReport(pout, con, "400", "Bad Request", 
-					"Your Browser sent a request that this server could not understand.");
-		} else {
-			String req = request.substring(4, request.length()-9).trim();
-			if (req.indexOf("/.")!= -1 || req.endsWith("~")) {
-				errorReport(pout, con, "403", "Forbidden",
-					"You don't have permission to access the requested URL.");
-			} else {
-				String path = wwwhome+"/"+req;
-				File f = new File(path);
-				if (f.isDirectory() && !path.endsWith("/")) {
-					pout.print("HTTP/1.0 301 Moved Permanently\r\n"+
-							"Location: http://"+ 
-							con.getLocalAddress().getHostAddress()+":"+
-							con.getLocalPort()+req+"/\r\n\r\n");
-					log(con, "301 Moved Permanently");
-				} else {
-					if (f.isDirectory()) {
-						path = path+"index.html";
-						f = new File(path);
-					}
-				}
-				try {
-					InputStream file = new FileInputStream(f);
-					String contenttype = URLConnection.guessContentTypeFromName(path);
-					pout.print("HTTP/1.0 200 OK\r\n");
-					if (contenttype!=null) 
-						pout.print("Content-Type: "+contenttype+"\r\n");
-					pout.print("Date: "+new Date() + "\r\n"+
-							"Server: IXWT FileServer 1.0\r\n\r\n");
-					sendFile(file, out); // send raw file
-					log(con, "200 OK");
-				} catch (FileNotFoundException e) {
-					errorReport(pout, con, "404", "Not Found",
-							"The requested URL was not found on this server.");
-				}
-			}
-		} */
 	}
 	
 	private boolean isValidRequest(String request){
@@ -227,20 +184,14 @@ public class HTTPServer {
 	private void errorReport(PrintStream pout, Socket con, String code, String title, String msg) {
 		pout.print("HTTP/1.0 "+code+" "+title+"\r\n"+
 				"\r\n"+
-				"<!DOCTYPE HTML PUBLIC \"-//IETF//DTD HTML 2.0//EN\">\r\n"+
-				"<head><title>"+code+" "+title+"</title>\r\n"+
-				"</head><body>\r\n"+
-				"<h1>"+title+"</h1>\r\n"+msg+"<p>\r\n"+
-				"<hr><address>IXWT FileServer 1.0 at "+
+				"<!DOCTYPE html>\r\n"+
+				"<head>\r\n<title>"+code+" "+title+"</title>\r\n"+
+				"</head>\r\n<body>\r\n"+
+				"<h1>"+title+"</h1>\r\n<p>"+msg+"</p>\r\n"+
+				"<hr>\r\n<address>dDist HTTPServer 1.0 at "+
 				con.getLocalAddress().getHostName()+
 				" Port"+con.getLocalPort()+"</address>\r\n"+
-				"</body></html>\r\n");
+				"</body>\r\n</html>\r\n");
 		log(con, code+" "+title);
-	}
-	
-	private void sendFile(InputStream file, OutputStream out) throws IOException {
-		byte[] buffer = new byte[1000];
-		while (file.available()>0) 
-			out.write(buffer, 0, file.read(buffer));
 	}
 }
