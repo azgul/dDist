c4f1c0bda516aebc2d6900ffc9b1dea336dde883
Lars D. Rasmussen <ldr@cs.au.dk>
2012-01-30 12:33:01 +0100

    - Implementation of ShellFileProcessingStrategy

--- src/week1/ShellFileProcessingStrategy.java
+++ src/week1/ShellFileProcessingStrategy.java
@@ -4,8 +4,12 @@
  */
 package week1;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.HashMap;
+import java.util.Map;
 
 /**
  *
@@ -14,13 +18,28 @@ import java.util.HashMap;
 public class ShellFileProcessingStrategy implements FileProcessingStrategy {
 
 	@Override
-	public String processFile(File f) {
-		
-	}
-
-	@Override
 	public String processFile(File f, HashMap<String, String> args) {
-		throw new UnsupportedOperationException("Not supported yet.");
+		ProcessBuilder pb = new ProcessBuilder("/Users/larss/Desktop/ddist-wwwroot/shell-script.sh");
+		Map<String, String> env = pb.environment();
+		
+		for(String k : args.keySet()){
+			env.put(k, args.get(k));
+		}
+		
+		try{
+			Process p = pb.start();
+			BufferedReader r = new BufferedReader(new InputStreamReader(p.getInputStream()));
+			StringBuilder sb = new StringBuilder();
+			String line;
+			while((line = r.readLine()) != null)
+				sb.append(line);
+				
+			return line;
+		}catch (IOException e){
+			// IOException
+		}
+		
+		return "";
 	}
 	
 }
