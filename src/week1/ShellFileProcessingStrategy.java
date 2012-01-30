0882d04ea25cdac86b04d59adaedbf3203944a9f
Lars D. Rasmussen <ldr@cs.au.dk>
2012-01-30 11:45:22 +0100

    - Changed interface to reflect GET/POST string - Added ShellFileProcessingStrategy

---
+++ src/week1/ShellFileProcessingStrategy.java
@@ -0,0 +1,26 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package week1;
+
+import java.io.File;
+import java.util.HashMap;
+
+/**
+ *
+ * @author larss
+ */
+public class ShellFileProcessingStrategy implements FileProcessingStrategy {
+
+	@Override
+	public String processFile(File f) {
+		
+	}
+
+	@Override
+	public String processFile(File f, HashMap<String, String> args) {
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+	
+}
