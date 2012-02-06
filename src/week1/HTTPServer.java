80d4ccec663870562711fcead367ac12ee290df0
Randi Hillerøe <silwing@gmail.com>
2012-02-06 11:43:56 +0100

    Tried to do general solution. Not working out well.

--- src/week1/HTTPServer.java
+++ src/week1/HTTPServer.java
@@ -2,7 +2,9 @@ package week1;
 
 import java.net.*;
 import java.io.*;
+import java.lang.reflect.Method;
 import java.util.*;
+import week2.*;
 /**
  *
  * @author Martin
@@ -102,8 +104,9 @@ public class HTTPServer {
 			FileProcessingStrategy strategy;
 			if(fileStrategies.containsKey(inputtype))
 				strategy = fileStrategies.get(inputtype);
-			else
+			else{
 				strategy = fileStrategies.get("default");
+			}
 			String contenttype = strategy.getContentType(f);
 			
 			pout.print("HTTP/1.0 200 OK\r\n");
@@ -115,9 +118,62 @@ public class HTTPServer {
 			log(con, "200 OK");
 		}
 		catch(FileNotFoundException e){
-			errorReport(pout, con, "404", "Not Found",
+			if(!processAsObject(path, map)){
+				errorReport(pout, con, "404", "Not Found",
 							"The requested URL was not found on this server.");
+			}
 		}
+	}
+	
+	private boolean processAsObject(String[] path, Map<String,String> map){
+		try {
+			// See if an objects matches
+			String[] pathParts = path[0].split("/");
+			String cls = pathParts[0];
+			String method = pathParts[2];
+			int id = Integer.getInteger(pathParts[1]);
+			if(objects.containsKey(cls))
+			{
+				Object o;
+				if(objects.get(cls).size() >= id)
+					o = objects.get(cls).get(id);
+				else
+					return false;
+				
+				Map<String,Object> args = parseMapTypes(map);
+				Method m = o.getClass().getMethod(method, getArgTypes(args));
+				m.invoke(o, (Object)args.values());
+			}
+		}catch(Exception e){
+			return false;
+		}
+		
+		return false;
+	}
+	
+	private Class[] getArgTypes(Map<String,Object> map){
+		Class[] classes = new Class[map.size()];
+		int i = 0;
+		for(Map.Entry<String,Object> arg : map.entrySet()){
+			classes[i] = arg.getValue().getClass();
+			i++;
+		}
+		return classes;
+	}
+	
+	private Map<String,Object> parseMapTypes(Map<String,String> map){
+		Map<String,Object> ret = new HashMap<String,Object>();
+		
+		for(Map.Entry<String,String> arg : map.entrySet()){
+			try{
+				double d = Double.parseDouble(arg.getValue());
+				ret.put(arg.getKey(), d);
+			}catch(NumberFormatException e){
+				ret.put(arg.getKey(), arg.getValue());
+			}
+		}
+		
+		return ret;
 	}
 	
 	private void doPost(String request) {
