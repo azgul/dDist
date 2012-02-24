f20218cb12d76ccafb80c84d18830957bcc4a752
Martin <Martin@azgul>
2012-02-25 00:41:09 +0100

    Added some waits.. And it works.. Randomly.

--- test/TotallyOrderedMultiCastStressTest.java
+++ test/TotallyOrderedMultiCastStressTest.java
@@ -17,7 +17,7 @@ import week4.multicast.messages.AbstractLamportMessage;
  */
 public class TotallyOrderedMultiCastStressTest{
 	private int port = 1337;
-	private int peers = 3;
+	private int peers = 5;
 	private int passes = 100;
 	private ChatQueue[] queue;
 	
@@ -34,9 +34,7 @@ public class TotallyOrderedMultiCastStressTest{
 			for (int i=1; i<peers; i++) {
 				queue[i].joinGroup(port+1, new InetSocketAddress(InetAddress.getLocalHost(), port), MulticastQueue.DeliveryGuarantee.TOTAL);
 				port++;
-				try {
-					Thread.sleep(5000);
-				} catch (InterruptedException e) {}
+				wait(1);
 			}
 			
 		} catch (IOException e) {}
@@ -44,40 +42,17 @@ public class TotallyOrderedMultiCastStressTest{
 	}
 	
 	@Test
-	public void doesItWork() {
-		try{
-			Thread.sleep(1000);
-		}catch(InterruptedException e){
-			System.err.println("Interrupted...");
-			return;
-		}
-		
+	public void doesItWork() {		
 		int x = 0;
 		for (int i=0; i<passes;i++) {
-			for (int j=peers-1; j>=0; j--) {
+			for (int j=0; j<peers; j++) {
 				queue[j].put(Integer.toString(x)); 
 				x++;
 			}
 		}
 		System.out.println(x + " messages were sent");
 		
-		try {
-			Thread.sleep(1000);
-		} catch (InterruptedException e) {}
-		
-		/*System.out.println("Stopped sleeping");
-		for (int i=0;i<peers;i++) {
-			//System.out.println("queue: " + i);
-			PriorityQueue<AbstractLamportMessage> pendingGets = new PriorityQueue<AbstractLamportMessage>(queue[i].pendingGets);
-		
-			AbstractLamportMessage msg = pendingGets.poll();
-			while (msg!=null) {
-				//System.out.println(msg);
-				msg = pendingGets.poll();
-			}
-		}*/
-		
-		
+		wait(peers*3);		
 		
 		for (int i=0; i<(passes*peers);i++) {			
 			AbstractLamportMessage curr = null;
@@ -89,41 +64,29 @@ public class TotallyOrderedMultiCastStressTest{
 				while (!queue[j].shouldHandleMessage(curr))
 					curr = queue[j].get();
 				
-				
 				if (prev!=null) {
-					//System.out.println("#"+j+" Comparing '"+prev+"' to '"+curr+"': "+prev.toString().equals(curr.toString()));
-					System.out.println("#"+j+" Comparing '"+prev+"'("+prev.getClock()+") to '"+curr+"' ("+curr.getClock()+"): "+prev.toString().equals(curr.toString()));
+					//System.out.println("#"+j+" Comparing '"+prev+"'("+prev.getClock()+") to '"+curr+"' ("+curr.getClock()+"): "+prev.toString().equals(curr.toString()));
 					assertEquals(curr.toString(), prev.toString());
-					
-					//System.out.println("Peer "+(j+1)+" received: "+curr);
 				}
 				
 				prev=curr;
-				
 			}
 		}
 		
-		
-		try {
-			Thread.sleep(1000);
-		} catch (InterruptedException e) {}
-		
-		/*for (int i=0;i<peers;i++) {
-			HashSet<InetSocketAddress> hasConnectionToUs = new HashSet<InetSocketAddress> (queue[i].hasConnectionToUs);
-			System.out.println("Connections: " + queue[i].hasConnectionToUs.size());
-			
-			
-			for (InetSocketAddress inet : hasConnectionToUs)
-				System.out.println(queue[i].myAddress.getPort() + " is connected to from " + inet.getPort());
-		}*/
-		
-		
-		for (int i=0; i<peers; i++) 
+		wait(1);
+		/*
+		for (int i=0; i<peers; i++) {
 			queue[i].leaveGroup();
-		
+			wait(1);
+		}*/
+	}
+	
+	public static void wait(int secs) {
 		try {
-			for (int i=0; i<peers; i++)
-				queue[i].join();
-		} catch (InterruptedException e) {}
+			Thread.currentThread().sleep(secs*1000);
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.exit(-1);
+		}
 	}
 }
