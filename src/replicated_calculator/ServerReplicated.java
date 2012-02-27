438d1bbec0f4114c91e17064da5f6bf1cf3f27dc
Randi Hillerøe <silwing@gmail.com>
2012-02-27 11:07:08 +0100

    Reverted ServerStandalone, added ServerReplicated with changes.

--- src/replicated_calculator/ServerReplicated.java
+++ src/replicated_calculator/ServerReplicated.java
@@ -4,52 +4,75 @@ import java.io.IOException;
 import java.math.BigInteger;
 import java.util.HashMap;
 import java.net.InetSocketAddress;
+import multicast.MulticastQueue;
+import multicast.MulticastQueue.DeliveryGuarantee;
+import week6.multicast.CalculatorQueue;
+import week4.multicast.messages.AbstractLamportMessage;
+import week6.multicast.messages.ClientEventMessage;
 
 /**
  * 
- * An stand-along implementation of a server. It simply keeps the variables in 
+ * A stand-alone implementation of a server. It simply keeps the variables in 
  * a hash map and executes the commands in the natural way on this hash map.
  * 
  * @author Jesper Buus Nielsen, Aarhus University, 2012.
  *
  */
 
-public class ServerStandalone extends Thread implements ClientEventVisitor, Server {
+public class ServerReplicated extends Thread implements ClientEventVisitor, Server {
 
     protected final HashMap<String,BigInteger> valuation 
 	= new HashMap<String,BigInteger>();
     protected final HashMap<String,PointToPointQueueSenderEndNonRobust<ClientEvent>> clients 
 	= new HashMap<String,PointToPointQueueSenderEndNonRobust<ClientEvent>>();
+	protected CalculatorQueue queue;
     
     public void createGroup(int port) {
-	operationsFromClients = null;
-	try {
-	    operationsFromClients = new PointToPointQueueReceiverEndNonRobust<ClientEvent>();
-	    operationsFromClients.listenOnPort(Parameters.serverPortForClients);
-	} catch (IOException e) {
-	    System.err.println("Cannot start server!");
-	    System.err.println("Check your network connection!");
-	    System.err.println("Check that no other service runs on port " + Parameters.serverPortForClients + "!");
-	    System.err.println();
-	    System.err.println(e);
-	    System.exit(-1);
-	}		
-	this.start();
+		operationsFromClients = null;
+		try {
+			operationsFromClients = new PointToPointQueueReceiverEndNonRobust<ClientEvent>();
+			operationsFromClients.listenOnPort(Parameters.serverPortForClients);
+			queue = new CalculatorQueue();
+			queue.createGroup(Parameters.serverPortForServers, DeliveryGuarantee.TOTAL);
+		} catch (IOException e) {
+			System.err.println("Cannot start server!");
+			System.err.println("Check your network connection!");
+			System.err.println("Check that no other service runs on port " + Parameters.serverPortForClients + "!");
+			System.err.println();
+			System.err.println(e);
+			System.exit(-1);
+		}		
+		this.start();
     }
     
     public void joinGroup(InetSocketAddress knownPeer) {
-	throw new UnsupportedOperationException("joinGroup");
+		operationsFromClients = null;
+		try {
+			operationsFromClients = new PointToPointQueueReceiverEndNonRobust<ClientEvent>();
+			operationsFromClients.listenOnPort(Parameters.serverPortForClients);
+			CalculatorQueue queue = new CalculatorQueue();
+			queue.joinGroup(Parameters.serverPortForServers, knownPeer, DeliveryGuarantee.TOTAL);
+			
+		} catch (IOException e) {
+			System.err.println("Cannot start server!");
+			System.err.println("Check your network connection!");
+			System.err.println("Check that no other service runs on port " + Parameters.serverPortForClients + "!");
+			System.err.println();
+			System.err.println(e);
+			System.exit(-1);
+		}		
+		this.start();
     }
     
     /**
      * No group to leave, so simply shutsdown.
      */
     public void leaveGroup() {
-	operationsFromClients.shutdown();
-	for (String client : clients.keySet()) {
-	    clients.remove(client).shutdown();
-	}
-	
+		queue.leaveGroup();
+		operationsFromClients.shutdown();
+		for (String client : clients.keySet()) {
+			clients.remove(client).shutdown();
+		}
     }
     
     protected BigInteger valuate(String var) {
@@ -164,9 +187,13 @@ public class ServerStandalone extends Thread implements ClientEventVisitor, Serv
      * Sub-classes have to implement the visiting methods.
      */
     public void run() {
-		ClientEvent nextOperation;
-		while ((nextOperation = operationsFromClients.get())!=null) {
-			nextOperation.accept(this);
+		ClientEvent nextOperation = null;
+		ClientEventMessage msg = null;
+		while ((msg = queue.get())!=null || (nextOperation = operationsFromClients.get())!=null) {
+			if(msg != null)
+				msg.getClientEvent().accept(this);
+			if(nextOperation != null)
+				nextOperation.accept(this);
 		}
     }
     
