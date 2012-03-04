e292e9b163d865ba5f3b26246fb1613f11ff3938
Martin <Martin@azgul>
2012-03-04 03:40:43 +0100

    tried to make shit work but shit doesn't work

--- test/CalculatorTest.java
+++ test/CalculatorTest.java
@@ -44,7 +44,7 @@ public class CalculatorTest {
 		try {
 			for (int i=1; i<servers; i++) {
 				p++;
-				System.out.println("Joining with server " + i + " server port " + p + " and client port " + (p+1));
+				//System.out.println("Joining with server " + i + " server port " + p + " and client port " + (p+1));
 				server[i].joinGroup(p, new InetSocketAddress(InetAddress.getLocalHost(), port), p+1);
 				p++;
 				wait(1);
@@ -54,11 +54,11 @@ public class CalculatorTest {
 			int c = 0;
 			for (int i=0; i<servers; i++) {
 				for (int j=0; j<clients; j++) {
-					System.out.println("Connecting with: " + c + " on server " + i + " server port " + (p+1) + " with client port " + (p+100+c));
+					//System.out.println("Connecting with: " + c + " on server " + i + " server port " + (p+1) + " with client port " + (p+100+c));
 					if(client[c].connect(new InetSocketAddress(InetAddress.getLocalHost(), p+1), p+100+c, Integer.toString(c)))
-						System.out.println("Connected.");
-					else
-						System.out.println("Connection failed.");
+						//System.out.println("Connected.");
+					//else
+						//System.out.println("Connection failed.");
 					c++;
 				}
 				p++;
@@ -71,10 +71,9 @@ public class CalculatorTest {
 	System.out.println(str);
     }
 	
-	@Test
-	public void doesItWork() {
+	public void putNumbers(String[] vars) {
+		
 		Random r = new Random();
-		String[] vars = {"a","b","c","b","e","f"};
 		int c, var1, var2, var3, type;
 		BigInteger value;
 		
@@ -84,11 +83,11 @@ public class CalculatorTest {
 			var3 = r.nextInt(vars.length);
 			type = r.nextInt(2);
 			c = r.nextInt(client.length);
-			System.out.println("Type: " + type);
+			//System.out.println("Type: " + type);
 			switch(type){
 				// Add
 				case 1:
-					System.out.println("Adding: " + vars[var3] + " = " + vars[var1] + " + " + vars[var2]);
+					//System.out.println("Adding: " + vars[var3] + " = " + vars[var1] + " + " + vars[var2]);
 					client[c].add(vars[var1], vars[var2], vars[var3]);
 					
 					break;
@@ -97,11 +96,18 @@ public class CalculatorTest {
 				// Assign
 				default:
 					value = new BigInteger(8, r);
-					System.out.println("Assigning: " + vars[var1] + " = " + value);
+					//System.out.println("Assigning: " + vars[var1] + " = " + value);
 					client[c].assign(vars[var1], value);
 					break;
 			}
 		}
+	}
+	
+	@Test
+	public void doesItWork() {
+		String[] vars = {"a","b","c","b","e","f"};
+		
+		putNumbers(vars);
 		
 		wait(1);
 		
@@ -109,7 +115,59 @@ public class CalculatorTest {
 			curr = null;
 			prev = null;
 			for(int i = 0; i < client.length; i++){
-				final String v = s;
+				final String cl = "Client" + i + ": " + s + " = ";
+				client[i].read(s, new Callback<BigInteger>(){
+					public void result(BigInteger bi){
+						set(bi);
+					}
+				});
+				wait(1);
+				System.out.println(cl + curr);
+				
+				if (prev!=null)
+					assertEquals(curr, prev);
+				
+				prev=curr;
+			}
+			wait(1);
+		}
+	}
+	
+	//@Test
+	public void doesItWorkStill() {
+		String[] vars = {"a","b","c","b","e","f"};
+		
+		putNumbers(vars);
+		
+		for (int i=0; i<4; i++) {
+			client[i].disconnect();
+		}
+		wait(30);
+		server[0].leaveGroup();
+		server[0] = null;
+		server[0] = new ServerReplicated();
+		wait(30);
+		
+		int p = port;
+		
+		try {
+			p++;
+			System.out.println("Joining with server port " + p + " and client port " + (p+1));
+			server[0].joinGroup(port, new InetSocketAddress(InetAddress.getLocalHost(), p+1), port+1);
+		
+			wait(1);
+			for (int i=0; i<4; i++) {
+				client[i].connect(new InetSocketAddress(InetAddress.getLocalHost(), port+2), p+100+i, Integer.toString(i));
+				p=p+2;
+				wait(1);
+			}
+		
+		} catch (IOException e) {}
+		
+		for(String s : vars){
+			curr = null;
+			prev = null;
+			for(int i = 0; i < client.length; i++){
 				final String cl = "Client" + i + ": " + s + " = ";
 				client[i].read(s, new Callback<BigInteger>(){
 					public void result(BigInteger bi){
@@ -136,7 +194,7 @@ public class CalculatorTest {
 	
 	public static void wait(int secs) {
 		try {
-			Thread.currentThread().sleep(secs*1000);
+			Thread.currentThread().sleep(secs*300);
 		} catch (Exception e) {
 			e.printStackTrace();
 			System.exit(-1);
